4e997c28a97fac76ac05635b487bae20d407390f
petergaobro <pgao@isw.net.au>
2023-08-14 09:32:52 +1000

    feat: try to add one component in pdp

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,21 +1,20 @@
-import React, { Suspense, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-const Header = React.lazy(() => import("home/Header"));
-// import Header from "home/Header";
+import Header from "home/Header";
 import Footer from "home/Footer";
+import SafeComponent from "./SafeComponent";
 
 const App = () => {
-  const [showHeader, setShowHeader] = useState(true)
   return (
-    < div className="text-3xl mx-auto max-w-6xl" >
-      <Suspense fallback={<div>Loading</div>}>
+    <div className="text-3xl mx-auto max-w-6xl">
+      <SafeComponent>
         <Header />
-      </Suspense>
+      </SafeComponent>
       <div className="my-10">
-        PDP page
+        PDP pageddd
       </div>
       <Footer />
     </div >
